2f642d13bdcf27abda93bf5fa8b3511f36eb3aef
Mikhail Khazov <mkhazov.work@gmail.com>
2017-01-11 19:52:23 +0300

    import videojs-vtt.js

--- src/bundle.js
+++ src/bundle.js
@@ -1,4 +1,5 @@
 import 'expose-loader?videojs!video.js';
+import 'expose-loader?WebVTT!videojs-vtt.js'
 import 'videojs-contrib-ads/src/videojs.ads';
 import 'videojs-ima';
 import 'videojs-ga/dist/videojs.ga.js';
--- src/hls-js/index.js
+++ src/hls-js/index.js
@@ -1,4 +1,5 @@
 import '../bundle';
+import 'hls.js';
 import 'videojs5-hlsjs-source-handler/lib/main';
 import wjplayer from '../wjplayer';
 export default wjplayer;
