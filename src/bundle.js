4f8b97be50534ab88d64a3d05a3c52f8e8f7e758
Mikhail Khazov <mkhazov.work@gmail.com>
2017-06-14 12:03:29 +0300

    replay button

--- src/bundle.js
+++ src/bundle.js
@@ -4,5 +4,6 @@ import 'videojs-ima';
 import 'videojs-ga/dist/videojs.ga.js';
 import 'videojs-share/dist/videojs-share.cjs';
 import 'videojs-download-button';
+import 'videojs-replay';
 
 import './i18n';
